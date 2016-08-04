e33e0aca55ef859944d08685ca693c7eaf06cd02
boeyd1 <dominic.boey@gmail.com>
2016-08-04 12:40:45 +0800

    HI JORRIE!

--- BarBarGo/ViewController.swift
+++ BarBarGo/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate{
+
+    @IBAction func arrowButtonPressed(sender: AnyObject) {
+        costSegmentedControl.hidden = !costSegmentedControl.hidden
+    
+    }
+    
+    @IBOutlet weak var costSegmentedControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        costSegmentedControl.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    
 
 }
 
