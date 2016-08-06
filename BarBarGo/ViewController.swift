dbd3fa0eeb3c96998ae35a66cceab1e63e2cdb07
boeyd1 <dominic.boey@gmail.com>
2016-08-06 23:36:50 +0800

    changed storyboard and added core location and map

--- BarBarGo/ViewController.swift
+++ BarBarGo/ViewController.swift
@@ -11,7 +11,7 @@ import MapKit
 import CoreLocation
 
 
-class ViewController: UIViewController, MKMapViewDelegate{
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     @IBOutlet weak var arrowButton: UIButton!
 
     @IBAction func arrowButtonPressed(sender: AnyObject) {
@@ -22,13 +22,34 @@ class ViewController: UIViewController, MKMapViewDelegate{
     
     @IBOutlet weak var costSegmentedControl: UISegmentedControl!
     
+    let locationManager = CLLocationManager()
+    var currLocation : CLLocation? {
+        didSet{
+            
+            let coordinate = currLocation?.coordinate
+            
+            let region = MKCoordinateRegionMakeWithDistance(coordinate!, 1000, 1000)
+            
+            self.mapView.setRegion(region, animated: false)
+
+        }
+    }
+    @IBAction func leftButtonTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func rightButtonTapped(sender: AnyObject) {
+        locationManager.requestLocation()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         costSegmentedControl.hidden = true
-        
-        
+        self.mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestLocation()
         
     }
 
@@ -37,8 +58,13 @@ class ViewController: UIViewController, MKMapViewDelegate{
         // Dispose of any resources that can be recreated.
     }
     
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currLocation = locations.last
+        print("updating location")
+    }
     
-    
-
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error.localizedDescription)
+    }
 }
 
