528a4323616bf37adf105b69691d9086b652f2b7
boeyd1 <dominic.boey@gmail.com>
2016-08-08 00:08:12 +0800

    added new VC, added routing,
    problem with updating labels in parent vc

---
+++ BarBarGo/RouteViewController.swift
@@ -0,0 +1,229 @@
+//
+//  RouteViewController.swift
+//  BarBarGo
+//
+//  Created by Desmond Boey on 7/8/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreLocation
+import MapKit
+import SwiftyJSON
+import YelpAPI
+
+class RouteViewController: UIViewController, MKMapViewDelegate{
+     @IBOutlet weak var routePickerView: UIPickerView!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var barNameLabel: UILabel!
+    @IBOutlet weak var ratingsLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
+    var directionData : [String] = [String]()
+    var client: YLPClient!
+    var locationManager = CLLocationManager()
+    var walkingRoute : MKRoute!
+    
+    var counter = 0
+    let searchLimit = 20
+    
+    var currentLocation: CLLocation?
+    let updateDistance : Double = 50 //distance in meters user must move for update to trigger
+    
+    var shouldUpdateTempLocation = true
+    var tempLocation: CLLocation? {
+        didSet{
+            runYelpSearch()
+        }
+    }
+    
+    var newYelpBusinesses : [YLPBusiness]! {
+        didSet{
+            //include codes that will notify users that there are updated entries; updated when location changes
+        }
+    }
+    
+    var yelpBusinesses : [YLPBusiness]! {
+        didSet{
+            //update the view
+            refreshMap()
+        
+        }
+    }
+    
+   
+   
+    @IBOutlet weak var moreInfoButton: UIButton!
+    
+    func refreshMap(){
+        
+        let yelpBusiness = yelpBusinesses[counter]
+        barNameLabel.text = yelpBusiness.name
+        ratingsLabel.text = "\(yelpBusiness.rating)⭐️ | \(yelpBusiness.reviewCount)"
+        
+        
+        let walkingRouteRequest = MKDirectionsRequest()
+        
+        walkingRouteRequest.source = MKMapItem.mapItemForCurrentLocation()
+        
+        walkingRouteRequest.transportType = MKDirectionsTransportType.Walking
+        
+        let barLocation = yelpBusiness.location.coordinate
+        
+        let barLocationCoordinates2D = CLLocationCoordinate2D(latitude:  (barLocation?.latitude)!, longitude: (barLocation?.longitude)!)
+        
+        let barLocationPlacemark = MKPlacemark(coordinate: barLocationCoordinates2D, addressDictionary: nil)
+        
+        let destination = MKMapItem(placemark: barLocationPlacemark)
+        
+        walkingRouteRequest.destination = destination
+        
+        let walkingRouteDirections = MKDirections(request: walkingRouteRequest)
+        
+        walkingRouteDirections.calculateDirectionsWithCompletionHandler { (response: MKDirectionsResponse?, error: NSError?) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            
+            let overlays = self.mapView.overlays
+            self.mapView.removeOverlays(overlays)
+            
+            self.walkingRoute = response!.routes[0]
+            self.mapView.addOverlay(self.walkingRoute.polyline, level: MKOverlayLevel.AboveRoads)
+            
+            self.mapView.setVisibleMapRect(self.walkingRoute.polyline.boundingMapRect, edgePadding: UIEdgeInsetsMake(35.0, 35.0, 35.0, 35.0) , animated: true)
+            
+            for routeStep in self.walkingRoute.steps{
+                self.directionData.append("\(routeStep.instructions) in \(Int(routeStep.distance))m")
+            }
+            self.routePickerView.reloadAllComponents()
+        }
+    }
+
+    
+    
+    @IBAction func moreInfoButtonTapped(sender: AnyObject) {
+        //performSegueWithIdentifier(<#T##identifier: String##String#>, sender: <#T##AnyObject?#>)
+        
+        //moreInfoButton should change to map icon
+        //unwinding should change back to ... icon
+    }
+    
+    
+    @IBAction func refreshButtonTapped(sender: AnyObject) {
+        runYelpSearch()
+        counter = 0
+        
+        refreshMap()
+    }
+    
+    @IBAction func nextBarButtonTapped(sender: AnyObject) {
+        if counter == searchLimit {
+            counter = 0
+        }else{
+        counter += 1
+        }
+        refreshMap()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        client = YLPClient(consumerKey: CONSUMER_KEY, consumerSecret: CONSUMER_SECRET, token: YELP_TOKEN, tokenSecret: YELP_TOKEN_SECRET)
+        locationManager.distanceFilter = updateDistance
+        locationManager.startUpdatingLocation()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func runYelpSearch(){
+        let lat = Double((currentLocation?.coordinate.latitude)!)
+        let lon = Double((currentLocation?.coordinate.longitude)!)
+        let alt = Double((currentLocation?.altitude)!)
+        
+        let coordinate = YLPGeoCoordinate(latitude: lat, longitude: lon, accuracy: 1000, altitude: alt, altitudeAccuracy: alt)
+        let location = YLPCoordinate(latitude: lat, longitude: lon)
+        
+        client.searchWithGeoCoordinate(coordinate, currentLatLong: location, term: "bar", limit: UInt(searchLimit), offset: 0, sort: YLPSortType.Distance) { (results: YLPSearch?, error:NSError?) -> Void in
+            if error != nil{
+                print(error)
+                return
+            }
+            
+            if let results = results {
+                print(results)
+                for restaurant in results.businesses{
+                    print(restaurant.name)
+                }
+                self.yelpBusinesses = results.businesses
+            }
+            
+            
+        }
+        
+    }
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        let draw = MKPolylineRenderer(overlay: overlay)
+        draw.strokeColor = UIColor.purpleColor()
+        draw.lineWidth = 3.0
+        return draw
+    }
+    
+    
+}
+
+extension RouteViewController: CLLocationManagerDelegate{
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        currentLocation = locations.last //get the last location in the array
+        
+        if shouldUpdateTempLocation {
+            tempLocation = locations.last
+            shouldUpdateTempLocation = false
+        }
+        
+        //include code here to run yelp search but saved to newYelpBusinesses
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error.localizedDescription)
+        
+    }
+    
+}
+
+extension RouteViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return directionData.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
+        let pickerLabel = UILabel()
+        let titleData = directionData[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSForegroundColorAttributeName: UIColor.blackColor()])
+        pickerLabel.textAlignment = .Left
+        pickerLabel.adjustsFontSizeToFitWidth = true
+        pickerLabel.attributedText = myTitle
+        pickerLabel.numberOfLines = 3
+        return pickerLabel
+    }
+
+}
