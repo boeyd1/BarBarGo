8c09c85e53eeb4926164f33f2d816062bd8f0e3f
boeyd1 <dominic.boey@gmail.com>
2016-08-10 22:22:10 +0800

    okay, added this time

---
+++ BarBarGo/Helpers/NotificationHelper.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationHelper.swift
+//  BarBarGo
+//
+//  Created by Desmond Boey on 9/8/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import Foundation
+
+
+class NotificationHelper {
+    
+    static let notification = NSNotificationCenter.defaultCenter()
+    static let ntfnBroadcast = "reviewSet"
+    
+}
